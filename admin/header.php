f93b6d966644d0d26d1bab5ac9b63d8ca76b0f26
Emmanuel <emmanuel.evg2001@gmail.com>
2025-07-06 21:55:22 -0400

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -9,7 +9,7 @@ $currentPage = basename($_SERVER['PHP_SELF']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Administrador - 4E Bazar</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
-    <link rel="stylesheet" href="/admin/css_admin/admin_styles.css">
+    <link rel="stylesheet" href="css_admin/admin_styles.css">
 </head>
 <body>
 <div class="admin-wrapper">
